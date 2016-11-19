5f76fd5b29eb15e7aa2ede74009302f6d6b5af49
Koule7b <Koule7b@gmail.com>
2016-11-19 08:00:05 +0100

    Initial commit

---
+++ src/Dravec.java
@@ -0,0 +1,5 @@
+/**
+ * Created by stepanmudra on 19.11.16.
+ */
+public class Dravec extends Korist{
+}
---
+++ src/Spusteni.java
@@ -0,0 +1,5 @@
+/**
+ * Created by stepanmudra on 06.11.16.
+ */
+public class Spusteni {
+}
